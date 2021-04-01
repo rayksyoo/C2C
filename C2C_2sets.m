e4e27ea5b2d75a78077442cb922efdcd5702698f
Kwangsun Ray Yoo <rayksyoo@users.noreply.github.com>
2021-04-01 14:00:20 -0400

    Update C2C_2sets.m

--- C2C_2sets.m
+++ C2C_2sets.m
@@ -1,48 +1,45 @@
 function matPred = C2C_2sets(conMat2useG1, conMat2predG1, conMat2useG2, nCompPCA, nCompPLS, meanPCA)
-% conMat : subjects x edges
+% conMat : subjects x edges (the number of subejcts by the number of 1-dim vectorized connectivity edges)
 
-if nargin < 4;    nCompPCA = [];    end;    
-if nargin < 5;    nCompPLS = [];    end;    
+if nargin < 4;    nCompPCA = 100;    end;    
+if nargin < 5;    nCompPLS = 10;    end;    
 if nargin < 6;    meanPCA = 0;    end;
 
-% nCompPCA=100;    nCompPLS = 10;    meanPCA = 0;
-
 %% Run C2C pipeline
-disp('    Running C2C transformation');
+disp('    Running C2C transformation ...');
 
-% ----  Construct a state transition model using a training set of group 1
+% ----  Construct a state transformation model in a training set (Group 1)
 
-    % Define subnetwork templates for each state in training set with PCA
+    % Define subnetworks for each state (resting state and task-related state) in training set with PCA
     if meanPCA == 0
         [s1coeff, s1score, ~, ~, ~, s1mu] = pca(conMat2useG1, 'Centered', false);        [s2coeff, s2score, ~, ~, ~, s2mu] = pca(conMat2predG1, 'Centered', false);
     elseif meanPCA == 1
         [s1coeff, s1score, ~, ~, ~, s1mu] = pca(conMat2useG1);        [s2coeff, s2score, ~, ~, ~, s2mu] = pca(conMat2predG1);
     end;    
 
-    % Select the number of top subnetwork to use for state transition
+    % Select the number (nCompPCA) of top subnetworks to use for state transformation
     if ~isempty(nCompPCA)
         s1score(:, [(nCompPCA+1):end]) = [];    s2score(:, [(nCompPCA+1):end]) = [];
         s1coeff(:, [(nCompPCA+1):end]) = [];    s2coeff(:, [(nCompPCA+1):end]) = [];
     end
     
-    % Define a function of state transformation with PLSR of specific number of PLSR components
+    % Define a state transformation with PLSR with a specific number of PLSR components (nCompPLS)
     if ~isempty(nCompPLS);        [~,~,~,~, betaPLS] = plsregress( s1score, s2score, nCompPLS);
     else;                                   [~,~,~,~, betaPLS] = plsregress( s1score, s2score);
     end;
 
-% ----  Apply the constructed transition model to a testing set of group 2
+% ----  Apply the constructed state transformation model to a testing set (Group 2)
 
-    % Estimate, in testing set, weights for the predefined subnetwork templates of state 1
+    % Estimate weights for the predefined subnetworks of state 1 (e.g., resting-state subnetworks) of a testing set
     if meanPCA == 0;              s1score2pred = conMat2useG2 / s1coeff';
     elseif meanPCA == 1;        s1score2pred = (conMat2useG2 - s1mu) / s1coeff';    end;
 
-    % Predict, in testing set, weights for the predefined subnetwork templates of state 2
-    s2scorePred = [ones(size(s1score2pred,1),1) s1score2pred]  * betaPLS;    clear beta
+    % Predict weights for the predefined subnetworks of state 2 (e.g., task-related subnetworks) of a testing set
+    s2scorePred = [ones(size(s1score2pred,1),1) s1score2pred]  * betaPLS;
 
-    % Predict, in testing set, the whole brain connectome of state 2
+    % Predict the whole brain connectome of state 2 of a testing set
     if meanPCA == 0;              matPred = (s2scorePred * s2coeff');
     elseif meanPCA == 1;        matPred = (s2scorePred * s2coeff') + s2mu;    
-    end;    clear s2scorePred
+    end
     
-disp('    ');
-
+disp('    Connectomes generated.');
